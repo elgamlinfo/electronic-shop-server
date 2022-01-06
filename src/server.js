6613f16420ae85af109a3693d61f2ab9f65a1a23
elgamlinfo <elgaml.info@gmail.com>
2022-01-06 17:49:06 +0200

    chache

--- src/server.js
+++ src/server.js
@@ -39,6 +39,22 @@ app.use(session({
 /*************end init app tools***********/
 
 
+app.use(express.static('public', {
+    etag: true, // Just being explicit about the default.
+    lastModified: true,  // Just being explicit about the default.
+    setHeaders: (res, path) => {
+      const hashRegExp = new RegExp('\\.[0-9a-f]{8}\\.');
+  
+      if (path.endsWith('.html')) {
+        // All of the project's HTML files end in .html
+        res.setHeader('Cache-Control', 'no-cache');
+      } else if (hashRegExp.test(path)) {
+        // If the RegExp matched, then we have a versioned URL.
+        res.setHeader('Cache-Control', 'max-age=31536000');
+      }
+    },
+  }));
+
 
 /********start  use routers*******/
 app.use(userRouter)
