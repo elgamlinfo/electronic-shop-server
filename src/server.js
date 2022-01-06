a530a294d35a53d57cb79e83e992afeca357683f
elgamlinfo <elgaml.info@gmail.com>
2022-01-06 17:25:59 +0200

    reset everything

--- src/server.js
+++ src/server.js
@@ -9,66 +9,12 @@ const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const session = require('express-session')
 const cookieParser = require('cookie-parser')
-var finalhandler = require('finalhandler')
-const serveStatic = require('serve-static')
-var http = require('http')
 const port = process.env.PORT || 3000;
 /********end  required files*******/
-
-
 /*************start init app tools***********/
-
-
-// Serve up public/ftp folder
-var serve = serveStatic('public/ftp', {
-    index: false,
-    setHeaders: setHeaders
-})
-
-// Create server
-var server = http.createServer(function onRequest (req, res) {
-    serve(req, res, finalhandler(req, res))
-})
-
-// Set header to force download
-function setHeaders (res, path) {
-    res.setHeader('Content-Disposition', contentDisposition(path))
-}
-
-server.on("request", function (request, response) {
-	console.log(`METHOD: ${request.method}; URL: ${request.url}`);
-	switch(request.method)
-	{
-	case "GET":
-	case "PUT":
-	case "POST":
-	case "PATCH":
-	case "DELETE":
-		response.writeHead(200, {
-			"Content-Type":"application/json",
-			"Access-Control-Allow-Origin":"*", // REQUIRED CORS HEADER
-			"Access-Control-Allow-Headers":"Origin, X-Requested-With, Content-Type, Accept" // REQUIRED CORS HEADER
-		});
-		response.end(JSON.stringify({dummy:"dummy"}));
-		break;
-	case "OPTIONS": // THE CLIENT OCCASIONALLY - NOT ALWAYS - CHECKS THIS
-		response.writeHead(200, {
-			"Access-Control-Allow-Origin":"*", // REQUIRED CORS HEADER
-			"Access-Control-Allow-Methods":"GET, POST, DELETE, PUT, PATCH", // REQUIRED CORS HEADER
-			"Access-Control-Allow-Headers":"Origin, X-Requested-With, Content-Type, Accept" // REQUIRED CORS HEADER
-		});
-		response.end();
-		break;
-	default:
-		response.writeHead(405, {
-			"Content-Type":"application/json"
-		});
-		response.end(JSON.stringify({error:`method ${request.method} not allowed`}));
-		break;
-	}
-});
-
 app.use(cors())
+
+
 /********start  required routers*******/
 const userRouter = require('./routes/user.route');
 const categoryRouter = require('./routes/category.route');
@@ -105,5 +51,5 @@ app.use(productRouter)
 
 
 /***********server listen************/
-server.listen(port, () => console.log(`http://localhost:${port}`))
+app.listen(port, () => console.log(`http://localhost:${port}`))
 /***********server listen************/
